25a2f62d0467ca4fb9caadc61f4e4fbf0eebf13a
YANWUMI <42022211+YANWUMI@users.noreply.github.com>
2019-12-17 09:57:14 +0800

    php连接数据库

---
+++ create.php
@@ -0,0 +1,28 @@
+<?php
+
+header("Content-Type: text/html;charset=utf-8");
+
+$servername = "localhost";
+$username = "root";
+$password = "root";
+
+try {
+    $conn = new PDO("mysql:host=$servername", $username, $password);
+
+    // 设置 PDO 错误模式为异常
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "CREATE DATABASE myDBPDO1";
+
+    // 使用 exec() ，因为没有结果返回
+    $conn->exec($sql);
+
+    echo "数据库创建成功<br>";
+}
+catch(PDOException $e)
+{
+    echo $sql . "<br>" . $e->getMessage();
+}
+
+$conn = null;
+
+?>
---
+++ welcome.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+	</head>
+	<body>
+
+欢迎 <?php echo $_POST["fname"]; ?>!<br>
+你的年龄是 <?php echo $_POST["age"]; ?>  岁。	
+	</form>	
+		
+				
+	</body>
+</html>
